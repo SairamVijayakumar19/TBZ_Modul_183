524925c9217c7efa033666008c690e4d032fb897
SairamVijayakumar19 <sairam.vijayakumar@noseryoung.com>
2025-03-30 18:57:21 +0200

    S1-Resolved-SQL-Injection

---
+++ lb2-applikation/todo-list-node/savetask.js
@@ -0,0 +1,41 @@
+const db = require('./fw/db');
+
+async function getHtml(req) {
+    let html = '';
+    let taskId = '';
+
+    // see if the id exists in the database
+    if (req.body.id !== undefined && req.body.id.length !== 0) {
+        taskId = req.body.id;
+        let stmt = await db.executeStatement('select ID, title, state from tasks where ID = ' + taskId);
+        if (stmt.length === 0) {
+            taskId = '';
+        }
+    }
+
+    if (req.body.title !== undefined && req.body.state !== undefined){
+        let state = req.body.state;
+        let title = req.body.title;
+        let userid = req.cookies.userid;
+
+        if (taskId === ''){
+            stmt = db.executeStatement(
+                "INSERT INTO tasks (title, state, userID) VALUES (?, ?, ?)",
+                [title, state, userid]
+              );
+        } else {
+            stmt = db.executeStatement(
+                "UPDATE tasks SET title = ?, state = ? WHERE ID = ?",
+                [title, state, taskId]
+              );
+        }
+
+        html += "<span class='info info-success'>Update successfull</span>";
+    } else {
+        html += "<span class='info info-error'>No update was made</span>";
+    }
+
+    return html;
+}
+
+module.exports = { html: getHtml }
---
+++ lb2-applikation/todo-list-node/search/v2/index.js
@@ -0,0 +1,27 @@
+const db = require('../../fw/db');
+
+async function search(req) {
+    if (req.query.userid === undefined || req.query.terms === undefined){
+        return "Not enough information to search";
+    }
+
+    let userid = req.query.userid;
+    let terms = req.query.terms;
+    let result = '';
+
+    let stmt = await db.executeStatement(
+        "SELECT ID, title, state FROM tasks WHERE userID = ? AND title LIKE ?",
+        [userid, `%${terms}%`]
+      );
+    if (stmt.length > 0) {
+        stmt.forEach(function(row) {
+            result += row.title+' ('+row.state+')<br />';
+        });
+    }
+
+    return result;
+}
+
+module.exports = {
+    search: search
+};
